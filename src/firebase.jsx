2b50a2c515c565ce080322e4459b8779cb7cfb26
sarthakpy7 <128068382+sarthakpy7@users.noreply.github.com>
2024-11-18 16:05:22 +0530

    commit

---
+++ src/firebase.jsx
@@ -0,0 +1,36 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBHqILdemr8fVLrThOTdnvG2cF8IpM-hJo",
+//   authDomain: "nexus-8487a.firebaseapp.com",
+//   projectId: "nexus-8487a",
+//   storageBucket: "nexus-8487a.appspot.com",
+//   messagingSenderId: "393295080767",
+//   appId: "1:393295080767:web:d4173802628ecb810fb78a",
+//   measurementId: "G-E7F1N4MBCK",
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyBZ5xAaQJAtKMf4eMbCiVvfCm399rfeBLc",
+  authDomain: "nexus-f8b26.firebaseapp.com",
+  projectId: "nexus-f8b26",
+  storageBucket: "nexus-f8b26.appspot.com",
+  messagingSenderId: "1062589630466",
+  appId: "1:1062589630466:web:ce74af628a92f78d127f1b",
+  measurementId: "G-TDEE92WWQC"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
